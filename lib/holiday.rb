647d58e90de74f96339f3e2a963c4bbb9e6edc92
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 21:48:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, val|
-    puts key.to_s.capitalize
+    puts key.to_s.capitalize + ":"
     val.each do |key2, val2|
       val2.map!(&:capitalize)
       puts key2.to_s.capitalize + ": " + val2.join(", ")
