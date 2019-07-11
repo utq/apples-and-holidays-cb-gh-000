cb008a7eba8bc4004794cc45fe7644d7f05659e3
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 21:37:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_hash[season.to_sym] = {holiday_name.to_sym: supply_array}
+  holiday_hash[season.to_sym] = {holiday_name.to_sym => supply_array}
   holiday_hash
 end
 
